03f6a38c1645ab40fc127562194872ca97b68ea5
van_loechtern_felix <jeffry.crap@gmx.de>
2022-02-17 15:00:14 +0100

    Redisign Save and Load Mechanic. One String with Hours and MInuts Speperated by Doppelpunkt instead of two diffrent Strings

--- src/main/java/com/example/lueftungsplan/Alarm.java
+++ src/main/java/com/example/lueftungsplan/Alarm.java
@@ -4,11 +4,28 @@ import java.time.LocalDateTime;
 
 public class Alarm {
     LocalDateTime alarmZeitpunkt;
+    String alarmZeitpunktString;
 
     public Alarm() {}
 
     public Alarm(LocalDateTime alarmZeitpunkt) {
         this.alarmZeitpunkt = alarmZeitpunkt;
+        this.alarmZeitpunktString = getZeitpunktStringFromLocalDateTime(alarmZeitpunkt);
+    }
+
+    public String getZeitpunktStringFromLocalDateTime (LocalDateTime ldt) {
+        String stundeString = ldt.getHour() + "";
+        String minuteString = ldt.getMinute() + "";
+
+        if (ldt.getHour() < 10) {
+            stundeString = "0" + stundeString;
+        }
+
+        if(ldt.getMinute() < 10) {
+            minuteString = "0" + minuteString;
+        }
+
+        return stundeString + ":" + minuteString;
     }
 
     public LocalDateTime getAlarmZeitpunkt() {
@@ -17,5 +34,13 @@ public class Alarm {
 
     public void setAlarmZeitpunkt(LocalDateTime alarmZeitpunkt) {
         this.alarmZeitpunkt = alarmZeitpunkt;
+    }
+
+    public String getAlarmZeitpunktString() {
+        return alarmZeitpunktString;
+    }
+
+    public void setAlarmZeitpunktString() {
+        this.alarmZeitpunktString = getZeitpunktStringFromLocalDateTime(alarmZeitpunkt);
     }
 }
